735940eded63f85e02e7c0bd19e9ea5aa9655111
Luis Davi <luisdavi.contato@gmail.com>
2023-05-05 16:58:55 -0300

    test

--- middleware/admin.js
+++ middleware/admin.js
@@ -2,5 +2,6 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     const {ip} = await $fetch("https://api.ipify.org/?format=json", {
         method: 'GET'
     })
+    console.log(ip)
     return ip == "177.37.201.21" ? true : navigateTo('/')
   })
